81a6bef8e98611345f1e6caac9e5cedf1751177f
DealSense 100 <dealsense@example.com>
2026-03-16 08:13:31 +0100

    Add /packages page with PricingAccordion - ultraminimalistyczny design

---
+++ app/packages/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import PricingAccordion from '../components/PricingAccordion'
+
+export default function PackagesPage() {
+  return (
+    <div style={{
+      minHeight: '100vh',
+      background: '#F7F9F8',
+      padding: '20px 0'
+    }}>
+      <PricingAccordion />
+    </div>
+  )
+}
