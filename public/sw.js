68911af0e14adc4dee206d44d157e9c1209ddb48
Mario Gorski <gorski1981mario@gmail.com>
2026-03-27 15:23:39 +0100

    Fix PWA manifest: add id, remove shortcuts/screenshots, add service worker, use PNG icons

---
+++ public/sw.js
@@ -0,0 +1,39 @@
+// Service Worker for DealSense PWA
+const CACHE_NAME = 'dealsense-v1';
+const urlsToCache = [
+  '/',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch event - network first, fallback to cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+  );
+});
+
+// Activate event - clean old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
