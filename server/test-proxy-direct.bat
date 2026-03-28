5e667f158f7f12cb9208353019e86f545e0f4b82
Mario Gorski <gorski1981mario@gmail.com>
2026-03-28 11:44:39 +0100

    Clean deployment: remove backup folders, HTML previews, docs from git (files preserved on disk)

--- server/test-proxy-direct.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Testing IPRoyal proxy with curl...
-curl -x "http://rd60wbshpxgDWD3F:1gYsyrIt8Vwk27RQ_country-nl_city-amsterdam_session-Q4og2CbM_lifetime-30m@geo.iproyal.com:12321" -s https://api.ipify.org?format=json
-echo.
